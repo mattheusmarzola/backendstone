5fd84c1d51d44f2d98556d190f61efa15cd8cea0
Mattheus <mattheusrml@gmail.com>
2019-12-07 12:06:14 -0300

    fixing auth_pipeline name, routes to sign_in and sign_up, get user and accounts

--- app/lib/backendstone_web/router.ex
+++ app/lib/backendstone_web/router.ex
@@ -1,23 +1,31 @@
 defmodule BackendstoneWeb.Router do
   use BackendstoneWeb, :router
 
+  alias Backendstone.UserManager.AuthPipeline
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  # We use ensure_auth to fail if there is no one logged in
-  pipeline :ensure_auth do
-    plug Backendstone.UserManager.Pipeline
+  pipeline :jwt_authenticated do
+    plug AuthPipeline
     plug Guardian.Plug.EnsureAuthenticated
   end
 
-  scope "/api", BackendstoneWeb do
+  scope "/api/v1", BackendstoneWeb do
     pipe_through :api
 
-    scope "/v1", BackendstoneWeb do
-      pipe_through [:ensure_auth]
-    
-      get "/protected", PageController, :protected
-    end
+    post "/sign_up", UserController, :create
+    post "/sign_in", SessionController, :login
+
+
+  end
+
+
+  scope "/api/v1", BackendstoneWeb do
+    pipe_through [:api, :jwt_authenticated]
+
+    get "/users", UserController, :show
+    resources "/accounts", AccountController, only: [:show]
   end
 end
