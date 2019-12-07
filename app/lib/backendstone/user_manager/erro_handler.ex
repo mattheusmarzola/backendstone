b032b5c7dd8b122e25b67ccb8024231fc500d462
Mattheus <mattheusrml@gmail.com>
2019-12-07 11:59:08 -0300

    fixed response to json

--- app/lib/backendstone/user_manager/erro_handler.ex
+++ app/lib/backendstone/user_manager/erro_handler.ex
@@ -2,13 +2,11 @@
 defmodule Backendstone.UserManager.ErrorHandler do
   import Plug.Conn
 
-  @behaviour Guardian.Plug.ErrorHandler
+  import Plug.Conn
 
-  @impl Guardian.Plug.ErrorHandler
   def auth_error(conn, {type, _reason}, _opts) do
-    body = to_string(type)
-    conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(401, body)
+    body = Jason.encode!(%{error: to_string(type)})
+    send_resp(conn, 401, body)
   end
 end
+
