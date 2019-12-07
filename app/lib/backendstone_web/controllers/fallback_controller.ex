e2d5b960deb6a5b5b6dc30c2ee315bb2044ab24a
Mattheus <mattheusrml@gmail.com>
2019-12-07 12:02:57 -0300

    added :unauthorized call

--- app/lib/backendstone_web/controllers/fallback_controller.ex
+++ app/lib/backendstone_web/controllers/fallback_controller.ex
@@ -19,4 +19,10 @@ defmodule BackendstoneWeb.FallbackController do
     |> put_view(BackendstoneWeb.ErrorView)
     |> render(:"404")
   end
+
+  def call(conn, {:error, :unauthorized}) do
+    conn
+    |> put_status(:unauthorized)
+    |> json(%{error: "Login error"})
+  end
 end
