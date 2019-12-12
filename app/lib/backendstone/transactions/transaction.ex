d92172dc15afb8bf146eeedc523f8bdf87513677
Mattheus <mattheusrml@gmail.com>
2019-12-11 22:53:57 -0300

    renaming account_to to target_account

--- app/lib/backendstone/transactions/transaction.ex
+++ app/lib/backendstone/transactions/transaction.ex
@@ -7,19 +7,21 @@ defmodule Backendstone.Transactions.Transaction do
   alias Backendstone.Transactions.TransactionStatus
 
   schema "transactions" do
-    field :account_to, :integer
     field :amount, :decimal
 
+    belongs_to :target_account, Account, foreign_key: :target_account_id
     belongs_to :account, Account, foreign_key: :account_id
+
     belongs_to :type, Type, foreign_key: :type_id
     belongs_to :transaction_status, TransactionStatus, foreign_key: :transaction_status_id
+
     timestamps()
   end
 
   @doc false
   def changeset(transaction, attrs) do
     transaction
-    |> cast(attrs, [:amount, :account_to, :type_id, :transaction_status_id, :account_id])
+    |> cast(attrs, [:amount, :target_account_id, :type_id, :transaction_status_id, :account_id])
     |> validate_required([:amount, :type_id, :transaction_status_id, :account_id])
   end
 end
